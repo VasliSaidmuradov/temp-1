61ba83e9ea7b0f39e09eb7aef65bba34400dd69b
Tamerlan <aytuarov01@gmail.com>
2020-03-15 18:15:28 +0600

    footer done

---
+++ components/partials/footer.vue
@@ -0,0 +1,37 @@
+<template>
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="footer__col">
+					<p class="footer__copyright">2020 © Skiny.kz интернет-магазин</p>
+					<img src="/images/cards.png" alt="Skiny image">
+				</div>
+				<div class="footer__col">
+					<h6 class="footer__heading">Компания</h6>
+					<nuxt-link to="" class="footer__link">О компании</nuxt-link>
+					<nuxt-link to="" class="footer__link">Новости</nuxt-link>
+					<nuxt-link to="" class="footer__link">Магазины</nuxt-link>
+				</div>
+				<div class="footer__col">
+					<h6 class="footer__heading">ПОМОЩЬ</h6>
+					<nuxt-link to="" class="footer__link">Вопрос-ответ</nuxt-link>
+					<nuxt-link to="" class="footer__link">Оплата</nuxt-link>
+					<nuxt-link to="" class="footer__link">Доставка</nuxt-link>
+				</div>
+				<div class="footer__col">
+					<h6 class="footer__heading">ИНФОРМАЦИЯ</h6>
+					<nuxt-link to="" class="footer__link">Бренды</nuxt-link>
+					<nuxt-link to="" class="footer__link">Акции</nuxt-link>
+					<nuxt-link to="" class="footer__link">Бонусы</nuxt-link>
+				</div>
+				<div class="footer__col">
+					<div class="footer__phoneWrp">
+						<a href="" class="footer__phone">8 (727) 396-18-34</a>
+						<a href="" class="footer__phone">8 (727) 396-18-34</a>
+						<button>Заказать звонок</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
     <div>
         <Header></Header>
         <nuxt />
+        <Footer />
     </div>
 </template>
 
 <script>
 import Header from '@/components/partials/header'
+import Footer from '@/components/partials/footer'
 export default {
     components: {
-        Header
+        Header,
+        Footer
     }
 }
 </script>

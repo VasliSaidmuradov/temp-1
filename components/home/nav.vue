2d8ae923810bf0c9958c3b32f7da673e8b9bb4c4
Tamerlan <aytuarov01@gmail.com>
2020-05-06 14:37:49 +0600

    nav subcategories

--- components/home/nav.vue
+++ components/home/nav.vue
@@ -5,11 +5,23 @@
       :key="category.id"
       class="home-nav-link"
       :to="category.slug"
+      @mouseover.native="onHoverCategory(category)"
     >
       <img src="/icons/city-icon.svg" alt="Skiny icon" />
       <span>{{ category.name }}</span>
     </nuxt-link>
     <!-- cat: {{ categories }} -->
+    <div class="home-nav-subcategory-wrp" v-if="hoveredCategory">
+      <div class="home-nav-subcategory-item"
+        v-for="subcategories in getSubCategoryChunks" 
+        :key="subcategories.id">
+        <nuxt-link class="home-nav-subcategory" to=""
+          v-for="subcategory in subcategories"
+          :key="subcategory.id">
+          {{subcategory.name}}
+        </nuxt-link>
+      </div>
+    </div>
   </nav>
 </template>
 
@@ -17,10 +29,37 @@
 import { mapGetters } from "vuex";
 
 export default {
+  data() {
+    return {
+      hoveredCategory: null
+    }
+  },
+  methods: {
+    onHoverCategory(category) {
+      if (category.subcategories && category.subcategories.length > 0) {
+        this.hoveredCategory = category
+      } else {
+        this.hoveredCategory = null
+      }
+    },
+    closeSubCategory() {
+      this.hoveredCategory = null
+    },
+  },
   computed: {
     ...mapGetters({
       categories: "menu/GET_CATEGORIES"
-    })
+    }),
+    getSubCategoryChunks: function() {
+      const chunked_arr = []
+      let copied = [...this.hoveredCategory.subcategories]
+      const size = 4
+      const numOfChild = Math.ceil(copied.length / size)
+      for (let i = 0; i < numOfChild; i++) {
+          chunked_arr.push(copied.splice(0, size))
+      }
+      return chunked_arr
+    }
   }
 };
 </script>

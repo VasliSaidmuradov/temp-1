16aa0e1db33f18a6f5a7bead7c9c510fd7de2b2d
Vasli Saidmuradov <vasli.said@gmail.com>
2020-06-17 02:49:54 +0600

    fix: async download all brands when go to category page from catalog page

--- middleware/brands.js
+++ middleware/brands.js
@@ -1,8 +1,7 @@
 export default async function ({ store, route }) {
   let queue = [
     store.dispatch('brand/fetchBrands'),
-    // store.dispatch('brand/fetchBrandProducts', route.params.brand)
   ]
 
-  await Promise.all(queue)
+  Promise.all(queue)
 }
